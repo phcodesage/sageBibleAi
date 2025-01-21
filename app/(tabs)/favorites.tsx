f746b64a64fdc0c52927daa32cb30d544c4601e2
Rechcel Toledo <rechceltoledo@gmail.com>
2025-01-21 20:52:59 +0800

    removed: unused sections

--- app/(tabs)/favorites.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function FavoritesScreen() {
-  return (
-    <View>
-      <Text>Favorites Screen</Text>
-    </View>
-  );
-} 
--- app/(tabs)/verses.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function VersesScreen() {
-  return (
-    <View>
-      <Text>Verses Screen</Text>
-    </View>
-  );
-} 
